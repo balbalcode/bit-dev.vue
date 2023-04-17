05fed400ac7bd801940d33680f70c7ff9ef35f96
balbalcode <iqbalmaulana027@gmail.com>
2023-04-18 05:16:22 +0700

    chore: initial commit

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<script>
+import appConfig from '@src/app.config'
+
+export default {
+  page: {
+    // All subcomponent titles will be injected into this template.
+    titleTemplate(title) {
+      title = typeof title === 'function' ? title(this.$store) : title
+      return title ? `${title} | ${appConfig.title}` : appConfig.title
+    },
+  },
+  components: {
+    ActiveButton: () => import('./components/atoms/ActiveButton/index.vue'),
+  },
+}
+</script>
+
+<template>
+  <div id="app"></div>
+</template>
+
+<!-- This should generally be the only global CSS in the app. -->
+<style lang="scss">
+@import '~normalize.css/normalize.css';
+@import '@design';
+</style>
